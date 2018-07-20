d4fa8b057c6ef83fb60f5e4b064221c00a2dc7ef
Jason Ryan <jryan@internal.onaware.com>
2018-07-20 11:34:57 +0100

    before adding service

---
+++ src/app/mock-heroes/mock-heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-heroes',
+  templateUrl: './mock-heroes.component.html',
+  styleUrls: ['./mock-heroes.component.css']
+})
+export class MockHeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
